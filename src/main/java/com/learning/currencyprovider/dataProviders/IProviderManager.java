d07b5d3a30764249331ec9dcdda89a524b8fb341
Konrad <konrad.hajduga@gmail.com>
2021-06-19 14:22:53 +0200

    delete unnecessary files

--- src/main/java/com/learning/currencyprovider/dataProviders/IProviderManager.java
+++
@@ -1,9 +0,0 @@
-package com.learning.currencyprovider.dataProviders;
-
-import com.learning.currencyprovider.dataProviders.api.APIResponse;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IProviderManager {
-    APIResponse getResponse(String baseCurrency, String quoteCurrency);
-}
--- src/main/java/com/learning/currencyprovider/dataProviders/api/IAPIConnector.java
+++
@@ -1,7 +0,0 @@
-package com.learning.currencyprovider.dataProviders.api;
-
-import org.json.JSONArray;
-
-public interface IAPIConnector {
-    JSONArray getResponse(String url);
-}
