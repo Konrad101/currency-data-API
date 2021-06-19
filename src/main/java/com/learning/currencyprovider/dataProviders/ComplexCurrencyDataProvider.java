6d711f2955260a3be62bba370df05054bf210b25
Konrad <konrad.hajduga@gmail.com>
2021-06-19 03:46:50 +0200

    add getting response from one source

--- src/main/java/com/learning/currencyprovider/dataProviders/ComplexCurrencyDataProvider.java
+++ src/main/java/com/learning/currencyprovider/dataProviders/ComplexCurrencyDataProvider.java
@@ -18,8 +18,6 @@ import java.util.*;
 public class ComplexCurrencyDataProvider implements ICurrencyDataProvider {
     private final List<IAPIDataProvider> apiProviders;
 
-    // bad idea
-    private String lastSource;
 
     public ComplexCurrencyDataProvider(@Qualifier("Http") IAPIConnector connector) {
         apiProviders = new ArrayList<>(Arrays.asList(
@@ -27,9 +25,6 @@ public class ComplexCurrencyDataProvider implements ICurrencyDataProvider {
                 new NbpCurrencyProvider(connector))
         );
     }
-
-    // class that will be able to decide to from what class get data
-    // when there won't be such a class, then return null
 
     @Override
     public void updateAvailableCurrencies() {
@@ -45,9 +40,78 @@ public class ComplexCurrencyDataProvider implements ICurrencyDataProvider {
 
     @Override
     public APIResponse getResponse(String baseCurrency, String quoteCurrency) {
+        IAPIDataProvider baseCurrencyProvider = getProviderForCurrency(baseCurrency);
+        IAPIDataProvider quoteCurrencyProvider = getProviderForCurrency(quoteCurrency);
+        if (baseCurrencyProvider == null || quoteCurrencyProvider == null) {
+            return getNotFoundResponse();
+        }
+
+        CurrencyPair requestedCurrencyPair;
+        if (baseCurrencyProvider == quoteCurrencyProvider) {
+            requestedCurrencyPair = baseCurrencyProvider.getRecentCurrencyRate(baseCurrency, quoteCurrency);
+        } else {
+            String commonProvidersCurrency = getCommonProvidersCurrency(baseCurrencyProvider, quoteCurrencyProvider);
+            if (commonProvidersCurrency == null) {
+                return getNotFoundResponse();
+            }
+            requestedCurrencyPair = getCurrencyValueFromProviders(
+                    baseCurrencyProvider, quoteCurrencyProvider,
+                    baseCurrency, quoteCurrency,
+                    commonProvidersCurrency);
+        }
+
+        return requestedCurrencyPair;
+        /*if (baseCurrencyProvider.getAvailableCurrencies().contains(quoteCurrency)) {
+            return baseCurrencyProvider.getRecentCurrencyRate(baseCurrency, quoteCurrency);
+        } else {
+            IAPIDataProvider quoteCurrencyProvider;
+            String commonProvidersCurrency;
+            for (IAPIDataProvider provider : apiProviders) {
+                if (provider != baseCurrencyProvider && provider.getAvailableCurrencies().contains(quoteCurrency)) {
+                    commonProvidersCurrency = getCommonProvidersCurrency(baseCurrencyProvider, provider);
+                    if (commonProvidersCurrency != null) {
+                        quoteCurrencyProvider = provider;
+                        break;
+                    }
+                }
+            }
+        }
+
         return new CurrencyPair(baseCurrency, quoteCurrency,
                 "Complex provider", new BigDecimal("1.23456"),
-                LocalDate.now());
+                LocalDate.now()
+        );*/
     }
 
+    private IAPIDataProvider getProviderForCurrency(String currency) {
+        currency = currency.toUpperCase();
+        for (IAPIDataProvider provider : apiProviders) {
+            if (provider.getAvailableCurrencies().contains(currency)) {
+                return provider;
+            }
+        }
+
+        return null;
+    }
+
+    private CurrencyPair getCurrencyValueFromProviders(IAPIDataProvider firstProvider, IAPIDataProvider secondProvider,
+                                                       String baseCurrency, String quoteCurrency, String commonCurrency) {
+
+        return null;
+    }
+
+    private APIResponse getNotFoundResponse() {
+        return null;
+    }
+
+    private String getCommonProvidersCurrency(IAPIDataProvider firstProvider, IAPIDataProvider secondProvider) {
+        Set<String> firstProviderCurrencies = firstProvider.getAvailableCurrencies();
+        for (String currency : firstProviderCurrencies) {
+            if (secondProvider.getAvailableCurrencies().contains(currency)) {
+                return currency;
+            }
+        }
+
+        return null;
+    }
 }
